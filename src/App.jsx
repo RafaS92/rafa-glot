efa6e414966ac2ecffcadba36308b797d81536af
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2025-08-28 12:56:51 -0500

    update image

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="app-container">
       <div className="banner-container">
-        <img alt="banner" src="public/world.png" className="banner" />
+        <img
+          alt="banner"
+          src="https://rafael-valdez-public.s3.us-east-1.amazonaws.com/image.png"
+          className="banner"
+        />
       </div>
       <TraslateForm></TraslateForm>
     </div>
