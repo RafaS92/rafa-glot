b3fb1013cebf5417130b4785050bf737158493bd
Rafael Salvador Valdez <manrvaldez.92@gmail.com>
2025-08-28 14:48:29 -0500

    updates

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 3001;
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
@@ -57,6 +58,4 @@ app.post("/api/image", async (req, res) => {
   }
 });
 
-app.listen(3001, () =>
-  console.log("✅ Server running on http://localhost:3001")
-);
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
